911d97ddd653aa147d10fc3617e99bbc50610b85
Sunny <lamyeungkong0108@gmail.com>
2026-05-29 23:11:26 +0800

    remove cache

--- config.sh
+++
@@ -1,8 +0,0 @@
-export PYTHONPATH=$PYTHONPATH:$PWD
-
-export ARK_API_BASE = "https://ark.ap-southeast.bytepluses.com/api/v3"
-export ARK_API_KEY = ""
-export AMAP_API_KEY = ""
-export AMAP_BASE_URL = "https://restapi.amap.com/v3"
-
-
--- src/schema/agent_response_schema.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel, Field 
-
-class AgentResponse(BaseModel):
-    reasoning: str = Field(..., description="The reasoning of calling the tool")
-    tool_name: str = Field(default="", description="The name of the tool to execute")
-    tool_args: dict = Field(default={}, description="The arguments to pass to the tool")
